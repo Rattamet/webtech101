5a3975a3683f487b467841e4495fa29a7526b06c
Rattamet <76036883+Rattamet@users.noreply.github.com>
2020-12-15 16:14:37 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -10,9 +10,10 @@
         </h1>
         <h2>Webtech for DS</h2>
 
-<p>หลักสูตรวิทยาศาสตรบัณฑิต สาขาวิชาวิทยาการข้อมูล <br>
-Bachelor of Science Program in Data Science</p>
+<p>หลักสูตรวิทยาศาสตรบัณฑิต สาขาวิชาวิทยาการข้อมูล </p>
+<p>Bachelor of Science Program in Data Science</p>
 
-<a href="https://cp.su.ac.th/curricula/Bachelor-of-Science-Program-in-Data-Science">Visit DSSU let's gooo</a>
+<a href="https://cp.su.ac.th/curricula/Bachelor-of-Science-Program-in-Data-Science">Visit DSSU let's gooo<br>
+<img src="https://cdn-images-1.medium.com/max/1280/1*1aW5u7BNK7fKVqm__viy4w.jpeg" width="500" height="600"></a>
     </body>
 </html>
